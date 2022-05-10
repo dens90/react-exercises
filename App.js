a2440c1daae2d78df1aff72c8602c5d113a94861
Dennis <dens90@hotmail.it>
2022-05-10 16:47:19 +0200

    component-03

---
+++ App.js
@@ -0,0 +1,11 @@
+import { Hello } from "./Hello";
+
+export class App extends React.Components{
+    render(){
+        return(
+            <div>
+                <Hello />
+            </div>
+        )
+    }
+}
