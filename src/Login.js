459bed549001ecb7490f91a4bd85b9394922273b
Dennis <dens90@hotmail.it>
2022-05-16 17:49:25 +0200

    Forms-02

--- src/Login.js
+++ src/Login.js
@@ -7,17 +7,31 @@ export class Login extends React.Component{
         remember: false
     }
     controlValue = (e) => {
+        const value = e.target.value;
+        const name = e.target.name
+        const checked = e.target.value
+        console.log(name,value,e)
         this.setState({
-            username: ''
-        })
+           [name]: value,
+            checked:value
+        })     
     }
-    setState
+    
+    controlCheck = (e) => {
+        const name = e.target.name;
+        const checked = e.target.checked
+        this.setState({
+            remember: checked,
+        })
+        console.log(checked)
+    }
     render(){
         return(
             <div>
-                <input  type="text" name="username"/>
-                 <input type="password" name="password"/> 
-                <input type="checkbox" name="remember"/>
+                <input  type="text" onChange={this.controlValue} name="username" value={this.state.username}/>
+                 <input type="password" name="password"  onChange={this.controlValue} value={this.state.value}/> 
+                <input type="checkbox" name="remember" checked={this.state.remember} onClick={this.controlCheck} />
+                
             </div>
         )
     }
