1855d588052e3f947d90c288971f5a8e5d57699e
Dennis <dens90@hotmail.it>
2022-05-16 16:14:08 +0200

    fix

--- src/Login.js
+++ src/Login.js
@@ -4,14 +4,19 @@ export class Login extends React.Component{
     state = {
         username: '',
         password: '',
-        remember: ''
+        remember: false
     }
-
+    controlValue = (e) => {
+        this.setState({
+            username: ''
+        })
+    }
+    setState
     render(){
         return(
             <div>
                 <input  type="text" name="username"/>
-                {/* <input type="password" name="password" onChange={}/> */}
+                 <input type="password" name="password"/> 
                 <input type="checkbox" name="remember"/>
             </div>
         )
