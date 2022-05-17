b53c50071967ba2b3726fea3da3d72de7010b53e
Dennis <dens90@hotmail.it>
2022-05-17 10:37:18 +0200

    Forms-03

--- src/Login.js
+++ src/Login.js
@@ -1,38 +1,72 @@
 import React from "react";
 
-export class Login extends React.Component{
-    state = {
-        username: '',
-        password: '',
-        remember: false
-    }
-    controlValue = (e) => {
-        const value = e.target.value;
-        const name = e.target.name
-        const checked = e.target.value
-        console.log(name,value,e)
-        this.setState({
-           [name]: value,
-            checked:value
-        })     
-    }
-    
-    controlCheck = (e) => {
-        const name = e.target.name;
-        const checked = e.target.checked
-        this.setState({
-            remember: checked,
-        })
-        console.log(checked)
-    }
-    render(){
-        return(
-            <div>
-                <input  type="text" onChange={this.controlValue} name="username" value={this.state.username}/>
-                 <input type="password" name="password"  onChange={this.controlValue} value={this.state.value}/> 
-                <input type="checkbox" name="remember" checked={this.state.remember} onClick={this.controlCheck} />
-                
-            </div>
-        )
-    }
+export class Login extends React.Component {
+  state = {
+    username: "",
+    password: "",
+    remember: false,
+    button: false,
+  };
+  controlValue = (e) => {
+    const value = e.target.value;
+    const name = e.target.name;
+    const checked = e.target.value;
+    this.setState({
+      [name]: value,
+      checked: value,
+      button: true,
+    });
+    console.log(e);
+  };
+
+  controlCheck = (e) => {
+    const name = e.target.name;
+    const checked = e.target.checked;
+    this.setState({
+      remember: checked,
+    });
+  };
+  onLogin = (e) => {
+    const value = e.target.value;
+    const name = e.target.name;
+    this.setState({
+      [name]: value,
+      button: true,
+    });
+  };
+  render() {
+    return (
+      <div>
+        <input
+          type="text"
+          onChange={this.controlValue}
+          name="username"
+          value={this.state.username}
+        />
+        <input
+          type="password"
+          name="password"
+          onChange={this.controlValue}
+          value={this.state.value}
+        />
+        <input
+          type="checkbox"
+          name="remember"
+          checked={this.state.remember}
+          onClick={this.controlCheck}
+        />
+        <button
+          onClick={this.onLogin}
+          value={
+            this.state.button
+              ? this.state.password.length >= 5 &&
+                this.state.username.length >= 5
+              : false
+          }
+        >
+          Submit
+        </button>
+      </div>
+    );
+  }
 }
