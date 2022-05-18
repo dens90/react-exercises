177c3bc846fe6332ac4045f45f9c7eb18bf44330
Dennis <dens90@hotmail.it>
2022-05-18 15:50:08 +0200

    component-composition-01

--- src/App.js
+++ src/App.js
@@ -1,31 +1,33 @@
-import React from "react"
-import { ClickCounter } from "./ClickCounter"
-import { ClickTracker } from "./ClickTracker"
-import { Counter } from "./Counter"
-import { CounterDisplay } from "./CounterDisplay"
-import { Hello } from "./Hello"
-import { InteractiveWelcome } from "./InteractiveWelcome"
-import { Login } from "./Login"
-import { TodoList } from "./TodoList"
-import { Uncontrolled, UncontrolledLogin } from "./UncontrolledLogin"
-import { Welcome } from "./Welcome"
+import React from "react";
+import { Container } from "./Container";
+import { ClickCounter } from "./ClickCounter";
+import { ClickTracker } from "./ClickTracker";
+import { Counter } from "./Counter";
+import { CounterDisplay } from "./CounterDisplay";
+import { Hello } from "./Hello";
+import { InteractiveWelcome } from "./InteractiveWelcome";
+import { Login } from "./Login";
+import { TodoList } from "./TodoList";
+import { Uncontrolled, UncontrolledLogin } from "./UncontrolledLogin";
+import { Welcome } from "./Welcome";
 
-
-export class App extends React.Component{
-    render(){
-        return(
-            <div>
-                <Hello name={<strong>React</strong>} />
-                <Welcome name='john' age={16}/>
-                <Counter />
-                <CounterDisplay  />
-                <ClickCounter />
-                <ClickTracker />
-                <InteractiveWelcome />
-                <Login />
-                <UncontrolledLogin />
-                <TodoList />
-            </div>
-        )
-    }
+export class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Container>
+          <Hello name={<strong>React</strong>} />
+          <Welcome name="john" age={16} />
+          <Counter />
+          <CounterDisplay />
+          <ClickCounter />
+          <ClickTracker />
+          <InteractiveWelcome />
+          <Login />
+          <UncontrolledLogin />
+          <TodoList />
+        </Container>
+      </div>
+    );
+  }
 }
