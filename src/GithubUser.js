1a57cee86a8246db270e8506b13445eab156611f
Dennis <dens90@hotmail.it>
2022-05-26 11:12:51 +0200

    custom-hooks-04

--- src/GithubUser.js
+++ src/GithubUser.js
@@ -1,9 +1,19 @@
+import { useEffect } from "react";
 import useGithubUsers from "./useGithubUser";
 
 const GithubUser = ({ username }) => {
-  const [data] = useGithubUsers(username);
+  const [data, loading, error, getDataFetch] = useGithubUsers(username);
 
-  return <div>{data && <h1>Name {data.name}</h1>}</div>;
+  const getFetch = () => {
+    getDataFetch(username);
+  };
+  
+  return (
+    <div>
+      <button onClick={getFetch}>Load user data</button>
+      {data && <h1>Name: {data.name}</h1>}
+    </div>
+  );
 };
 
 export default GithubUser;
--- src/useGithubUser.js
+++ src/useGithubUser.js
@@ -2,14 +2,27 @@ import { useEffect, useState } from "react";
 
 const useGithubUsers = (username) => {
   const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-  useEffect(() => {
-    fetch(`https://api.github.com/users/${username}`)
-      .then((response) => response.json())
-      .then((data) => setData(data));
-  }, [username]);
-
-  return [data];
+  async function getDataFetch(username) {
+    setLoading(true);
+    setError(null);
+    try {
+      const call = await fetch(`https://api.github.com/users/${username}`);
+      const response = await call.json();
+      setData(response);
+    } catch (error) {
+      setError(error);
+      setData(null);
+    } finally {
+      setLoading(false);
+    }
+  }
+  console.log(data);
+  console.log(error);
+  console.log(loading);
+  return [data, error, loading, getDataFetch];
 };
 
 export default useGithubUsers;
