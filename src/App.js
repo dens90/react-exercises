d80bb8f73c2077013a8c733254c48acaf683b226
Dennis <dens90@hotmail.it>
2022-05-28 17:21:24 +0200

    useContext

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Sum from "./Sum";
 import Counter from "./Counter";
 import Welcome from "./Welcome";
@@ -7,20 +7,25 @@ import GithubUser from "./GithubUser";
 import GithubUserList from "./GithubUserList";
 import FilteredList from "./FilteredList";
 import CarDetails from "./CarDetails";
+import { LanguageContext } from "./LanguageContext";
+import DisplayLanguage from "./DIsplayLanguage";
 
 const App = () => {
-  const onCounterChange = (count) => {
-    return count;
+  const [language, setLanguage] = useState("en");
+
+  const handleChangeLanguage = (e) => {
+    setLanguage(e.target.value);
   };
   return (
     <div>
-      <CarDetails initialData={{model:'', year:'', color:''}}/>
-      {/* <Sum /> */}
-      {/* <Counter /> */}
-      {/* <Form /> */}
-      {/* <GithubUser  username='dens90'/> */}
-      {/* <GithubUserList name /> */}
-      {/* <FilteredList /> */}
+      <LanguageContext.Provider value={language}>
+        <select value={language} onChange={handleChangeLanguage}>
+          <option value="en">English</option>
+          <option value="it">Italiano</option>
+        </select>
+
+        <DisplayLanguage />
+      </LanguageContext.Provider>
     </div>
   );
 };
