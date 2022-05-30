fc2a6b313f96ffa62488d833acddc5974222c3be
Dennis <dens90@hotmail.it>
2022-05-30 11:04:55 +0200

    fix

--- src/useGithubUser.js
+++ src/useGithubUser.js
@@ -1,42 +1,15 @@
-
 import useSWR from "swr";
 
-const fetcher = (url) => fetch(url).then((response) => response.json())
+const fetcher = (url) => fetch(url).then((response) => response.json());
 
 const useGithubUsers = () => {
+  const { data, error } = useSWR(`https://api.github.com/users`, fetcher);
 
-  const {data, error} = useSWR(`https://api.github.com/users`, fetcher)
-  
-return{
-  users:data,
-  error,
-  isLoading: !data && !error
-}
-
+  return {
+    users: data,
+    error,
+    isLoading: !data && !error,
+  };
 };
 
 export default useGithubUsers;
-
-
-// const [data, setData] = useState(null);
-// const [loading, setLoading] = useState(false);
-// const [error, setError] = useState(null);
-
-// async function getDataFetch(username) {
-//   setLoading(true);
-//   setError(null);
-//   try {
-//     const call = await fetch(`https://api.github.com/users/${username}`);
-//     const response = await call.json();
-//     setData(response);
-//   } catch (error) {
-//     setError(error);
-//     setData(null);
-//   } finally {
-//     setLoading(false);
-//   }
-// }
-// console.log(data);
-// console.log(error);
-// console.log(loading);
-// return [data, error, loading, getDataFetch];
