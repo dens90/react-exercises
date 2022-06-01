124fd088ab270304fd4dbe8544c0f9472bb0a534
Dennis <dens90@hotmail.it>
2022-06-01 14:38:54 +0200

    redux-02

---
+++ src/Store.js
@@ -0,0 +1,5 @@
+import { createStore } from "redux";
+
+import CounterReducer from "./CounterReducer";
+
+export const store = createStore(CounterReducer);
--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from "react";
-import ReactDOM from 'react-dom/client'
+import ReactDOM from "react-dom/client";
 import App from "./App";
-import './index.css'
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(
-    <React.StrictMode>
-            <App />
-    </React.StrictMode>
-)
+import { incrementCounter } from "./CounterReducer";
+import "./index.css";
+import { store } from "./Store";
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+
+//     <React.StrictMode>
+//             <App />
+//     </React.StrictMode>
+// )
+
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
+store.dispatch(incrementCounter(2));
+store.dispatch(incrementCounter(3));
